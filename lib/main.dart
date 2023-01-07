8616ab4b65b219dfcb5f65c334e9be0e40a208ba
MojitoBar <wnehdtjr5@gmail.com>
2023-01-07 22:54:56 +0900

    feat: promise list view added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              NavBarWidget(),
+              Divider(thickness: 1.0, color: Colors.grey),
+              PromiseList(),
+            ],
+          ),
+        ),
       ),
-      home: Text("Hello World"),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class NavBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: IconButton(
+        icon: Icon(
+          Icons.search,
+          size: 30,
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class PromiseList extends StatefulWidget {
+  @override
+  State<PromiseList> createState() => _PromiseListState();
+}
+
+class _PromiseListState extends State<PromiseList> {
+  List<String> items = ['Item1', 'Item2', 'Item3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: () {},
+            title: Container(child: Text(items[index])),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(thickness: 1);
+        },
+      ),
     );
   }
 }
